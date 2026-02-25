eeed20c6c61054e524a6c4d17280d4bb67380d7b
syara97 <yara0219@outlook.com>
2026-02-24 21:51:08 -0500

    Added 'HomePage' page, but modified route to make it easier to understand

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'; 
+import { Button, Card } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom';
+
+import { LanguageContext } from '../contexts/LanguageContext';
+
+const HomePage = () => {
+
+    const { changeLanguage } = useContext(LanguageContext); 
+    const performNavigation = useNavigate(); 
+
+    const performLanguageChange = (someLanguage, someRoute) => {
+        changeLanguage(someLanguage)
+        performNavigation(someRoute)
+    }
+
+    return <div className='d-grid gap-2'>
+        <Card className='text-center mb-3'>
+            <Card.Header as='h1' className='display-5 fw-bold'>IAMOOT 2025 - Language Selection</Card.Header>
+        </Card>
+
+        <Button variant='primary' onClick={() => (performLanguageChange('EN', '/written/login'))}>English</Button>
+        <Button variant='primary' onClick={() => (performLanguageChange('ES', '/written/login'))}>Español</Button>
+        <Button variant='primary' onClick={() => (performLanguageChange('POR', '/written/login'))}>Português</Button>
+    </div>
+}; 
+
+export default HomePage; 
